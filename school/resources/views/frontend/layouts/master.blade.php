adccc10092317a9f3674a080562f88ec988de99f
SAMIUL98SIAM <samiulsiam89@gmail.com>
2021-10-29 13:13:24 +0600

    frontend layouting

---
+++ school/resources/views/frontend/layouts/master.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Education &mdash; Free Website Template, Free HTML5 Template by freehtml5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by freehtml5.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="freehtml5.co" />
+
+	<!--
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE
+	DESIGNED & DEVELOPED by FreeHTML5.co
+
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	 -->
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400" rel="stylesheet">
+
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/animate.css')}}">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('/frontend/css/icomoon.css')}}">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/bootstrap.css')}}">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/magnific-popup.css')}}">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/owl.carousel.min.css')}}">
+	<link rel="stylesheet" href="{{asset('/frontend/css/owl.theme.default.min.css')}}">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/flexslider.css')}}">
+
+	<!-- Pricing -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/pricing.css')}}">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="{{asset('/frontend/css/style.css')}}">
+
+	<!-- Modernizr JS -->
+	<script src="{{asset('/frontend/js/modernizr-2.6.2.min.js')}}"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+
+	<div class="fh5co-loader"></div>
+
+	<div id="page">
+
+        <!----Navbar Part----->
+        @include('frontend.layouts.header')
+        <!----Navbar Part/---->
+
+
+        <!----Navbar Part----->
+        @include('frontend.layouts.slider')
+        <!----Navbar Part/---->
+
+
+        <!----Main Content------>
+        @yield('content')
+        <!----Main Content/----->
+
+
+        <!----footer Part---->
+        @include('frontend.layouts.footer')
+        <!----footer Part/--->
+
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+
+	<!-- jQuery -->
+	<script src="{{asset('/frontend/js/jquery.min.js')}}"></script>
+	<!-- jQuery Easing -->
+	<script src="{{asset('/frontend/js/jquery.easing.1.3.js')}}"></script>
+	<!-- Bootstrap -->
+	<script src="{{asset('/frontend/js/bootstrap.min.js')}}"></script>
+	<!-- Waypoints -->
+	<script src="{{asset('/frontend/js/jquery.waypoints.min.js')}}"></script>
+	<!-- Stellar Parallax -->
+	<script src="{{asset('/frontend/js/jquery.stellar.min.js')}}"></script>
+	<!-- Carousel -->
+	<script src="{{asset('/frontend/js/owl.carousel.min.js')}}"></script>
+	<!-- Flexslider -->
+	<script src="{{asset('/frontend/js/jquery.flexslider-min.js')}}"></script>
+	<!-- countTo -->
+	<script src="{{asset('/frontend/js/jquery.countTo.js')}}"></script>
+	<!-- Magnific Popup -->
+	<script src="{{asset('/frontend/js/jquery.magnific-popup.min.js')}}"></script>
+	<script src="{{asset('/frontend/js/magnific-popup-options.js')}}"></script>
+	<!-- Count Down -->
+	<script src="{{asset('/frontend/js/simplyCountdown.js')}}"></script>
+	<!-- Main -->
+	<script src="{{asset('/frontend/js/main.js')}}"></script>
+	<script>
+    var d = new Date(new Date().getTime() + 1000 * 120 * 120 * 2000);
+
+    // default example
+    simplyCountdown('.simply-countdown-one', {
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate()
+    });
+
+    //jQuery example
+    $('#simply-countdown-losange').simplyCountdown({
+        year: d.getFullYear(),
+        month: d.getMonth() + 1,
+        day: d.getDate(),
+        enableUtc: false
+    });
+	</script>
+	</body>
+</html>
+

daa9fbdc99af909f74b5436f86bc053e133104e7
SAMIUL98SIAM <samiulsiam89@gmail.com>
2021-10-30 18:02:05 +0600

    Backend Side Layouting

--- school/app/Http/Controllers/HomeController.php
+++ school/app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('admin.layouts.dashboard');
+        return view('admin.layouts.master.dashboard');
     }
 }
